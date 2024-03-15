48dbca511e8175e9f7b053ff2c2cd0eecccbde2c
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-03-15 16:36:20 +0100

    giggi

---
+++ ApplicazionePizzeria2.0/wwwroot/js/IncassoTotaleInDataSpecifica.js
@@ -0,0 +1,32 @@
+﻿const BtnOrdiniTot = document.getElementById("btnOrdiniEvasi");
+const btnTotaleIncassatoInData = document.getElementById("btnTotaleIncassatoInData");
+
+const MyFetch = () => {
+    fetch("/Ordine/FetchTotOrdniEvasi")
+        .then(response => response.json())
+        .then(data => {
+            pOutput = document.getElementById("outputElement");
+            pOutput.innerHTML = `Totale ordini evasi: ${data}`;
+        })
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    BtnOrdiniTot.addEventListener("click", () => {
+        MyFetch();
+    })
+
+    btnTotaleIncassatoInData.addEventListener("click", () => {
+
+        const inputDate = document.getElementById("idinputDate").value;
+
+        fetch(`/Ordine/FetchTotIncassatoInData?data=${inputDate}`)
+            .then(response => response.json())
+            .then(data => {
+                pOutput = document.getElementById("outputElement");
+                pOutput.innerHTML = `Totale incassato in data: ${data} €`;
+            })
+    })
+
+
+})
