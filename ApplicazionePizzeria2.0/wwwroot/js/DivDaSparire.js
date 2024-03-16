6af961497cca812672367d395dbb4e4d030c76fc
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-03-16 11:42:09 +0100

    easy animation

---
+++ ApplicazionePizzeria2.0/wwwroot/js/DivDaSparire.js
@@ -0,0 +1,10 @@
+﻿
+document.addEventListener("DOMContentLoaded", () => {
+    const divAnimazione = document.querySelectorAll(".heartbeat");
+
+    divAnimazione.forEach(divAnimato => {
+        setTimeout(() => {
+            divAnimato.style.display = "none";
+        }, 3000)
+    })
+});
